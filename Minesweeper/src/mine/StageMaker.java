dee774901ee992f768603863dea92174df3a673f
minju <alswn1589@gmail.com>
2019-05-23 16:03:30 +0900

    initial commit

---
+++ Minesweeper/src/mine/StageMaker.java
@@ -0,0 +1,20 @@
+package mine;
+
+public class StageMaker {
+
+	int row = 10; // 가로로 10칸 
+	int colum = 10;  // 세로로 10칸
+	
+	//정사각형 100칸 배열 생성
+	Integer[][] stage = new Integer[10][10];
+	
+	public StageMaker() {
+		makeMine();
+	}
+	
+	//지뢰 갯수는 10개
+	private void makeMine() {
+		
+	}
+	
+}
